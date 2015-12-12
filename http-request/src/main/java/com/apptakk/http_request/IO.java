b154fba880fcb55e7dc678a77125c026abe0c8fe
Erlend Fagerheim <erlendf80@gmail.com>
2015-12-12 13:49:04 +0100

    removed unused write method

--- http-request/src/main/java/com/apptakk/http_request/IO.java
+++ http-request/src/main/java/com/apptakk/http_request/IO.java
@@ -13,18 +13,11 @@ import java.io.OutputStreamWriter;
  */
 public class IO {
 
-    public static void write(OutputStream os, String body) throws IOException {
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
-        writer.write(body);
-        writer.flush();
-        writer.close();
-        os.close();
-    }
-
     public static void write(OutputStream os, byte[] body) throws IOException {
         os.write(body);
         os.close();
     }
+    
     public static String read(InputStream in) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
         StringBuilder sb = new StringBuilder();
